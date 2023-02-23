d3401db802341c004089d5c972d510c000b859ba
Deekshu3 <89259923+Deekshu3@users.noreply.github.com>
2023-02-23 15:18:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = '<img id="image"  src="'+data_uri+'">';
+});}
+
+console.log('ml5 version:',ml5.version);
+classifier = ml5.imageClassifier('https://drive.google.com/file/d/1wImQoMrVPIBptOeZn6GxwsMs-5BiLwx_/view/model.jason',modelLoaded);
+
+function check() {
+    img=document.getElementById("image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error); 
+    }
+    else{
+        console.log(results);
+        document.getElementById("display_object").innerHTML=results[0].label;
+        document.getElementById("display_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
